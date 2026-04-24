f5b0d1e2001e06df9c76ed9e59124944ec316fe5
Brandon Croft <brandon.croft@gmail.com>
2026-04-24 09:07:03 -0600

    go-tfe refresh

--- internal/pkg/client/helpers.go
+++ internal/pkg/client/helpers.go
@@ -2,47 +2,46 @@ package client
 
 import (
 	"github.com/hashicorp/go-tfe/api/models"
-	"github.com/hashicorp/go-tfe/api/organizations"
 )
 
 // NewVarRequest creates a new varsets.ItemRelationshipsVarsPostRequestBody
 // from parameters.
-func NewVarRequest(key, value, category string, sensitive bool) models.VarEnvelopeable {
+func NewVarRequest(key, value, category string, sensitive bool) models.VarsEnvelopeable {
 	hcl := false
-	attrib := &models.Var_attributes{}
+	attrib := &models.Vars_attributes{}
 	attrib.SetKey(&key)
 	attrib.SetValue(&value)
 	attrib.SetSensitive(&sensitive)
 	attrib.SetHcl(&hcl)
 	attrib.SetCategory(mustParseCategory(category))
 
-	data := &models.VarEscaped{}
+	data := &models.Vars{}
 	data.SetAttributes(attrib)
 
-	body := models.NewVarEnvelope()
+	body := models.NewVarsEnvelope()
 	body.SetData(data)
 
 	return body
 }
 
-func mustParseCategory(category string) *models.Var_attributes_category {
-	cat, err := models.ParseVar_attributes_category(category)
+func mustParseCategory(category string) *models.Vars_attributes_category {
+	cat, err := models.ParseVars_attributes_category(category)
 	if err != nil {
 		panic("cannot parse category \"" + category + "\"")
 	}
-	result := cat.(*models.Var_attributes_category)
+	result := cat.(*models.Vars_attributes_category)
 	return result
 }
 
 // NewOrganizationsVarsetsPostBody creates a new organizations.ItemVarsetsPostRequestBody
 // from parameters.
-func NewOrganizationsVarsetsPostBody(variableSetName string) *organizations.ItemVarsetsPostRequestBody {
-	attrib := &organizations.ItemVarsetsPostRequestBody_data_attributes{}
+func NewOrganizationsVarsetsPostBody(variableSetName string) *models.VarsetsEnvelope {
+	attrib := &models.Varsets_attributes{}
 	attrib.SetName(&variableSetName)
 
-	data := &organizations.ItemVarsetsPostRequestBody_data{}
+	data := &models.Varsets{}
 	data.SetAttributes(attrib)
-	body := &organizations.ItemVarsetsPostRequestBody{}
+	body := &models.VarsetsEnvelope{}
 	body.SetData(data)
 
 	return body
