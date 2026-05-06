a396e2aa55d0cdcc0b6d8a32dfabc4855bb19245
Chris Trombley <chris.trombley@hashicorp.com>
2026-05-06 10:25:56 -0700

    chore: use cli/browser to open token page

--- internal/commands/auth/browser.go
+++ internal/commands/auth/browser.go
@@ -3,23 +3,9 @@
 
 package auth
 
-import (
-	"os/exec"
-	"runtime"
-)
-
-// browserCmd returns the command and arguments for opening a URL in the default browser.
-func browserCmd(url string) (string, []string) {
-	switch runtime.GOOS {
-	case "darwin":
-		return "open", []string{url}
-	default:
-		return "xdg-open", []string{url}
-	}
-}
+import "github.com/cli/browser"
 
 // openBrowser attempts to open the given URL in the default browser.
 func openBrowser(url string) error {
-	bin, args := browserCmd(url)
-	return exec.Command(bin, args...).Start()
+	return browser.OpenURL(url)
 }
