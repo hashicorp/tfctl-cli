0604396dd81a5c77025e3242d44c3b7b309ab861
Shweta <35878561+shwetamurali@users.noreply.github.com>
2026-06-02 19:26:10 -0400

    check err

--- internal/pkg/client/client.go
+++ internal/pkg/client/client.go
@@ -123,6 +123,12 @@ func (c *Client) Do(ctx context.Context, req *Request) (*http.Response, error) {
 		return nil, err
 	}
 
+	if httpResp.StatusCode >= 400 {
+		if apiErr := tfe.APIErrorFactory(httpResp, nil); apiErr != nil {
+			return nil, apiErr
+		}
+	}
+
 	return httpResp, nil
 }
 
