a25fac666da4d1313e50ee086c4d87c7f112b7d8
Brandon Croft <brandon.croft@gmail.com>
2026-06-16 15:01:44 -0600

    anonymize hostname telemetry when not known

--- internal/pkg/profile/profile.go
+++ internal/pkg/profile/profile.go
@@ -257,6 +257,12 @@ func (p *Profile) GetHostname() string {
 	return p.Hostname
 }
 
+// IsHCPTerraform returns true if the profile's hostname is a known HCP Terraform hostname
+// in any geo.
+func (p *Profile) IsHCPTerraform() bool {
+	return p.GetHostname() == "app.terraform.io" || p.GetHostname() == "app.eu.terraform.io"
+}
+
 // SetDefaultOrganization sets the default organization.
 func (p *Profile) SetDefaultOrganization(name string) *Profile {
 	if p == nil {
