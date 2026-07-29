92ee0ebb513b4e38fc5f97d48578258ef968ffe9
Shweta <35878561+shwetamurali@users.noreply.github.com>
2026-07-29 11:35:05 -0700

    register destroy command

--- internal/commands/root/root.go
+++ internal/commands/root/root.go
@@ -10,6 +10,7 @@ import (
 	"github.com/hashicorp/tfctl-cli/internal/commands/api"
 	"github.com/hashicorp/tfctl-cli/internal/commands/auth"
 	"github.com/hashicorp/tfctl-cli/internal/commands/create"
+	"github.com/hashicorp/tfctl-cli/internal/commands/destroy"
 	"github.com/hashicorp/tfctl-cli/internal/commands/get"
 	"github.com/hashicorp/tfctl-cli/internal/commands/harness"
 	"github.com/hashicorp/tfctl-cli/internal/commands/profile"
@@ -41,6 +42,7 @@ func NewCmdRoot(inv *cmd.Invocation) *cmd.Command {
 	c.AddChild(api.NewCmdAPI(inv))
 	c.AddChild(get.NewCmdGet(inv))
 	c.AddChild(create.NewCmdCreate(inv))
+	c.AddChild(destroy.NewCmdDestroy(inv))
 	c.AddChild(run.NewCmdRun(inv))
 	c.AddChild(auth.NewCmdAuth(inv))
 	c.AddChild(variable.NewCmdVariable(inv))
